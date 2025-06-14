e448de026d4f8ffa26c0850fceabba622ae73139
Erwin <twlau1994@gmail.com>
2025-06-14 17:23:57 +0800

    Completed messages package.

---
+++ messages/messages.go
@@ -0,0 +1,44 @@
+package messages
+
+const (
+	Action_deposit                 int = 1
+	Action_withdraw                int = 2
+	Action_transfer                int = 3
+	Action_get_balance             int = 4
+	Action_get_transaction_history int = 5
+)
+
+type RedisMessage struct {
+	MessageID int64       `json:"id"`
+	Action    int         `json:"action"`
+	Body      interface{} `json:"body,omitempty"`
+}
+
+type POST_Deposit struct {
+	WalletID string `json:"wallet_id,omitempty"`
+	Amount   string `json:"amount,omitempty"`
+	Currency string `json:"currency,omitempty"`
+}
+
+type POST_Withdraw struct {
+	WalletID string `json:"wallet_id,omitempty"`
+	Amount   string `json:"amount,omitempty"`
+	Currency string `json:"currency,omitempty"`
+}
+
+type POST_Transfer struct {
+	SourceWalletID      string `json:"source_wallet_id,omitempty"`
+	DestinationWalletID string `json:"destination_wallet_id,omitempty"`
+	Amount              string `json:"amount,omitempty"`
+	Currency            string `json:"currency,omitempty"`
+}
+
+type GET_Balance struct {
+	WalletID string `json:"wallet_id,omitempty"`
+}
+
+type GET_TransactionHistory struct {
+	WalletID string `json:"wallet_id,omitempty"`
+	From     int    `json:"from,omitempty"` // YYYYMMDD
+	To       int    `json:"to,omitempty"`   // YYYYMMDD
+}
