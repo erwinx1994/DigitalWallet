615e0d92fd8eb26a2020037e5a8674301927f19b
Erwin <twlau1994@gmail.com>
2025-06-14 20:59:59 +0800

    Completed skeleton code for deposit service.

---
+++ deposit_service/implementation/deposit_service.go
@@ -0,0 +1,94 @@
+package implementation
+
+import (
+	"context"
+	"deposit_service/config"
+	"log"
+	"sync"
+	"sync/atomic"
+	"time"
+
+	"github.com/redis/go-redis/v9"
+)
+
+type DepositService struct {
+	config          *config.Config
+	is_alive        atomic.Bool
+	waitgroup       sync.WaitGroup
+	requests_queue  *redis.Client
+	responses_queue *redis.Client
+}
+
+func CreateDepositService(config *config.Config) *DepositService {
+	service := &DepositService{
+		config: config,
+	}
+	return service
+}
+
+func (service *DepositService) prepare_redis_clients() error {
+
+	background_context := context.Background()
+
+	{
+		// Prepare requests queue
+		requests_queue := redis.NewClient(service.config.RequestsQueue.GetRedisOptions())
+
+		timeout_context, cancel := context.WithTimeout(background_context, time.Duration(service.config.RequestsQueue.Timeout)*time.Second)
+		_, err := requests_queue.Ping(timeout_context).Result()
+		if err != nil {
+			cancel()
+			return err
+		}
+		cancel()
+
+		service.requests_queue = requests_queue
+	}
+
+	{
+		// Prepare responses queue
+		responses_queue := redis.NewClient(service.config.ResponsesQueue.GetRedisOptions())
+
+		timeout_context, cancel := context.WithTimeout(background_context, time.Duration(service.config.ResponsesQueue.Timeout)*time.Second)
+		_, err := responses_queue.Ping(timeout_context).Result()
+		if err != nil {
+			cancel()
+			return err
+		}
+		cancel()
+
+		service.responses_queue = responses_queue
+	}
+
+	return nil
+}
+
+func (service *DepositService) async_run() {
+	defer func() {
+		service.waitgroup.Done()
+		log.Println("Shutdown deposit service.")
+	}()
+
+	err := service.prepare_redis_clients()
+	if err != nil {
+		log.Fatal("Could not connect to Redis server: ", err)
+	}
+
+	// Service continues running until terminated by user
+	for service.is_alive.Load() {
+
+		log.Println("Started up deposit service.")
+		time.Sleep(1 * time.Second)
+	}
+}
+
+func (service *DepositService) Run() {
+	service.is_alive.Store(true)
+	service.waitgroup.Add(1)
+	go service.async_run()
+}
+
+func (service *DepositService) Shutdown() {
+	service.is_alive.Store(false)
+	service.waitgroup.Wait()
+}
--- deposit_service/main.go
+++ deposit_service/main.go
@@ -1,5 +1,40 @@
 package main
 
+import (
+	"deposit_service/config"
+	"deposit_service/implementation"
+	"log"
+	"os"
+	"os/signal"
+	"syscall"
+)
+
 func main() {
 
+	log.Println("DIGITAL WALLET INC DEPOSIT SERVICE")
+	log.Println("The deposit service creates a new deposit transaction for a wallet and updates its balance.")
+
+	// Load configuration file
+	config_file_path := "config.yml"
+	if len(os.Args) > 1 {
+		config_file_path = os.Args[1]
+	}
+	config, err := config.Load(config_file_path)
+	if err != nil {
+		log.Fatal("Unable to load configuration file at ", config_file_path)
+	}
+	log.Println("Successfully loaded configuration file at ", config_file_path)
+
+	// Start running deposit service
+	service := implementation.CreateDepositService(config)
+	service.Run()
+
+	// Listen for abort signal to terminate the balance service
+	// Pressing CTRL + C while the application is running
+	abort_channel := make(chan os.Signal, 1)
+	signal.Notify(abort_channel, syscall.SIGINT, syscall.SIGTERM, syscall.SIGQUIT)
+	<-abort_channel
+
+	// Shutdown the deposit service gracefully
+	service.Shutdown()
 }
