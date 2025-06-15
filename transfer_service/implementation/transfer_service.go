ac2a9bc56723e4747ecad3ce21f110b73052b2bd
Erwin <twlau1994@gmail.com>
2025-06-16 00:32:02 +0800

    Completed transfer service.

--- transfer_service/implementation/transfer_service.go
+++ transfer_service/implementation/transfer_service.go
@@ -4,14 +4,17 @@ import (
 	"context"
 	"database/sql"
 	"encoding/json"
+	"errors"
 	"log"
 	"shared/messages"
 	"shared/responses"
+	"shared/utilities"
 	"sync"
 	"sync/atomic"
 	"time"
 	"transfer_service/config"
 
+	_ "github.com/lib/pq"
 	"github.com/redis/go-redis/v9"
 )
 
@@ -67,6 +70,40 @@ func (service *TransferService) prepare_redis_clients() error {
 	return nil
 }
 
+func (service *TransferService) send_response(response_message *responses.Transfer) {
+	bytes_to_send, err := json.Marshal(response_message)
+	if err != nil {
+		log.Println("Failed to serialise response message. Should not happen in production.")
+		// In practice, we will need an error notification system. I have skipped
+		// building an error notification system due to time constraints.
+		return
+	}
+
+	// Put response into responses queue
+	timeout := time.Duration(service.config.ResponsesQueue.Timeout) * time.Second
+	queue_name := service.config.ResponsesQueue.QueueName
+	timeout_context, cancel := context.WithTimeout(service.background_context, timeout)
+	_, err = service.responses_queue.LPush(timeout_context, queue_name, bytes_to_send).Result()
+	if err != nil {
+		cancel()
+		log.Println("Failed to put response into responses queue.")
+		return
+	}
+	cancel()
+}
+
+func (service *TransferService) send_failed_response(message string, request_message *messages.POST_Transfer) {
+	response_message := responses.Transfer{
+		Header: responses.Header{
+			MessageID: request_message.Header.MessageID,
+			Action:    request_message.Header.Action,
+		},
+		Status:       responses.Status_failed,
+		ErrorMessage: message,
+	}
+	service.send_response(&response_message)
+}
+
 func (service *TransferService) async_run() {
 	defer func() {
 		service.waitgroup.Done()
@@ -94,6 +131,23 @@ func (service *TransferService) async_run() {
 	log.Println("Connected to PostgreSQL database.")
 
 	// Prepare commonly used SQL statements
+	get_currency_balance, err := db.Prepare("select currency, balance from " + service.config.WalletDatabase.BalanceTable + " where wallet_id=$1")
+	if err != nil {
+		log.Fatal("Unable to prepare SQL statement.")
+	}
+	defer get_currency_balance.Close()
+
+	insert_transaction, err := db.Prepare("insert into " + service.config.WalletDatabase.TransactionsTable + " (wallet_id, date_and_time, currency, amount) values ($1, $2, $3, $4)")
+	if err != nil {
+		log.Fatal("Unable to prepare SQL statement.")
+	}
+	defer insert_transaction.Close()
+
+	update_balance, err := db.Prepare("update " + service.config.WalletDatabase.BalanceTable + " set balance=$1 where wallet_id=$2")
+	if err != nil {
+		log.Fatal("Unable to prepare SQL statement.")
+	}
+	defer update_balance.Close()
 
 	// Service continues running until terminated by user
 	for service.is_alive.Load() {
@@ -124,38 +178,127 @@ func (service *TransferService) async_run() {
 
 		// Verify that the correct message was received
 		if request_message.Header.Action != messages.Action_transfer {
-			log.Println("Incorrect message received.")
+			service.send_failed_response("Message received by wrong service", &request_message)
 			continue
 		}
 
 		// Query PostgreSQL database
+		transaction_date_time := time.Now().UTC()
+		db_transaction, err := db.Begin()
+		if err != nil {
+			service.send_failed_response("Database error", &request_message)
+			continue
+		}
+
+		// Both the source and destination wallets must exist, otherwise return an error
+		var source_currency string = ""
+		var source_balance int64 = 0
+		tx_get_currency_balance := db_transaction.Stmt(get_currency_balance)
+		err = tx_get_currency_balance.QueryRow(request_message.SourceWalletID).Scan(&source_currency, &source_balance)
+		if err != nil {
+			db_transaction.Rollback()
+			if errors.Is(err, sql.ErrNoRows) {
+				service.send_failed_response("Source wallet does not exist", &request_message)
+			} else {
+				service.send_failed_response("Database error", &request_message)
+			}
+			continue
+		}
+
+		var destination_currency string = ""
+		var destination_balance int64 = 0
+		err = tx_get_currency_balance.QueryRow(request_message.DestinationWalletID).Scan(&destination_currency, &destination_balance)
+		if err != nil {
+			db_transaction.Rollback()
+			if errors.Is(err, sql.ErrNoRows) {
+				service.send_failed_response("Destination wallet does not exist", &request_message)
+			} else {
+				service.send_failed_response("Database error", &request_message)
+			}
+			continue
+		}
+
+		// The currency of the transfer must match the source and destination wallets.
+		// Otherwise return an error.
+		if source_currency != request_message.Currency {
+			db_transaction.Rollback()
+			service.send_failed_response("Transfer currency does not match currency of source wallet", &request_message)
+			continue
+		}
+		if destination_currency != request_message.Currency {
+			db_transaction.Rollback()
+			service.send_failed_response("Transfer currency does not match currency of destination wallet", &request_message)
+			continue
+		}
+
+		// The transfer amount must be less than or equal to the balance in the source wallet.
+		// Otherwise return an error.
+		transfer_amount, err := utilities.Convert_display_to_database_format(request_message.Amount)
+		if err != nil {
+			service.send_failed_response("Amount specified was invalid", &request_message)
+			continue
+		}
+		if transfer_amount > source_balance {
+			db_transaction.Rollback()
+			service.send_failed_response("Insufficient funds in source wallet", &request_message)
+			continue
+		}
+
+		// Add withdrawal transaction to source wallet
+		tx_insert_transaction := db_transaction.Stmt(insert_transaction)
+		_, err = tx_insert_transaction.Exec(request_message.SourceWalletID, transaction_date_time, request_message.Currency, -transfer_amount)
+		if err != nil {
+			db_transaction.Rollback()
+			service.send_failed_response("Database error", &request_message)
+			continue
+		}
+
+		// Add deposit transaction to destination wallet
+		_, err = tx_insert_transaction.Exec(request_message.DestinationWalletID, transaction_date_time, request_message.Currency, transfer_amount)
+		if err != nil {
+			db_transaction.Rollback()
+			service.send_failed_response("Database error", &request_message)
+			continue
+		}
+
+		// Update balance in source wallet
+		source_balance -= transfer_amount
+		tx_update_balance := db_transaction.Stmt(update_balance)
+		_, err = tx_update_balance.Exec(source_balance, request_message.SourceWalletID)
+		if err != nil {
+			db_transaction.Rollback()
+			service.send_failed_response("Database error", &request_message)
+			continue
+		}
+
+		// Update balance in destination wallet
+		destination_balance += transfer_amount
+		_, err = tx_update_balance.Exec(destination_balance, request_message.DestinationWalletID)
+		if err != nil {
+			db_transaction.Rollback()
+			service.send_failed_response("Database error", &request_message)
+			continue
+		}
+
+		// Commit database transaction
+		err = db_transaction.Commit()
+		if err != nil {
+			db_transaction.Rollback()
+			service.send_failed_response("Database error", &request_message)
+			continue
+		}
 
 		// Prepare response
-		response_message := responses.Deposit{
+		response_message := responses.Transfer{
 			Header: responses.Header{
 				MessageID: request_message.Header.MessageID,
 				Action:    request_message.Header.Action,
 			},
+			Status:     responses.Status_successful,
+			Currency:   request_message.Currency,
+			NewBalance: utilities.Convert_database_to_display_format(source_balance),
 		}
-		bytes_to_send, err := json.Marshal(response_message)
-		if err != nil {
-			log.Println("Failed to serialise response message. Should not happen in production.")
-			// In practice, we will need an error notification system. I have skipped
-			// building an error notification system due to time constraints.
-			continue
-		}
-
-		// Put response into responses queue
-		timeout = time.Duration(service.config.ResponsesQueue.Timeout) * time.Second
-		queue_name = service.config.ResponsesQueue.QueueName
-		timeout_context, cancel = context.WithTimeout(service.background_context, timeout)
-		_, err = service.responses_queue.LPush(timeout_context, queue_name, bytes_to_send).Result()
-		if err != nil {
-			cancel()
-			log.Println("Failed to put response into responses queue.")
-			return
-		}
-		cancel()
+		service.send_response(&response_message)
 
 	}
 }
--- transfer_service/implementation/transfer_service_test.go
+++ transfer_service/implementation/transfer_service_test.go
@@ -1,7 +1,212 @@
 package implementation
 
-import "testing"
+import (
+	"context"
+	"database/sql"
+	"encoding/json"
+	"log"
+	"reflect"
+	"shared/messages"
+	"shared/responses"
+	"testing"
+	"time"
+	"transfer_service/config"
+
+	"github.com/redis/go-redis/v9"
+)
 
 func Test_TransferService(t *testing.T) {
+
+	message_id := int64(283729)
+	currency := "SGD"
+
+	source_wallet_id := "source_wallet_id"
+	source_initial_balance := 10000
+
+	destination_wallet_id := "destination_wallet_id"
+	destination_initial_balance := 10000
+
+	transfer_amount := "10.00"
+	source_final_balance_str := "90.00"
+	source_final_balance := 9000
+	destination_final_balance := 11000
+
+	// Load default configuration file
+	config_file_path := "../config.yml"
+	config, err := config.Load(config_file_path)
+	if err != nil {
+		t.Fatal("Unable to load configuration file at ", config_file_path)
+	}
+
+	// Modify the table and message queue names
+	config.RequestsQueue.QueueName = "transfer_requests_queue_test"
+	config.ResponsesQueue.QueueName = "transfer_responses_queue_test"
+	config.WalletDatabase.BalanceTable = "postgres.test_transfer_service.balances"
+	config.WalletDatabase.TransactionsTable = "postgres.test_transfer_service.transactions"
+
+	// Start running balance service
+	service := CreateTransferService(config)
+	service.Run()
+	defer service.Shutdown()
+
+	// Prepare requests queue
+	background_context := context.Background()
+	requests_queue := redis.NewClient(config.RequestsQueue.GetRedisOptions())
+	timeout_context, cancel := context.WithTimeout(background_context, time.Duration(config.RequestsQueue.Timeout)*time.Second)
+	_, err = requests_queue.Ping(timeout_context).Result()
+	if err != nil {
+		cancel()
+		t.Fatal("Could not connect to requests queue.", err)
+	}
+	cancel()
+
+	// Prepare responses queue
+	responses_queue := redis.NewClient(config.ResponsesQueue.GetRedisOptions())
+	timeout_context, cancel = context.WithTimeout(background_context, time.Duration(config.ResponsesQueue.Timeout)*time.Second)
+	_, err = responses_queue.Ping(timeout_context).Result()
+	if err != nil {
+		cancel()
+		t.Fatal("Could not connect to responses queue.", err)
+	}
+	cancel()
+
+	// Prepare connection to PostgreSQL database
+	db, err := sql.Open("postgres", config.WalletDatabase.GetConnectionString())
+	if err != nil {
+		t.Fatal("Could not create PostgreSQL database object.", err)
+	}
+	defer db.Close()
+	err = db.Ping()
+	if err != nil {
+		t.Fatal("Could not connect to PostgreSQL database.", err)
+	}
+
+	// Create source wallet
+	{
+		transaction_date_time := time.Now().UTC()
+		_, err := db.Exec("insert into "+config.WalletDatabase.TransactionsTable+" (wallet_id, date_and_time, currency, amount) values ($1, $2, $3, $4)", source_wallet_id, transaction_date_time, currency, source_initial_balance)
+		if err != nil {
+			t.Fatal("Error updating database.", err)
+		}
+
+		_, err = db.Exec("insert into "+config.WalletDatabase.BalanceTable+" (wallet_id, currency, balance) values ($1, $2, $3)", source_wallet_id, currency, source_initial_balance)
+		if err != nil {
+			t.Fatal("Error updating database.", err)
+		}
+	}
+
+	// Create destination wallet
+	{
+		transaction_date_time := time.Now().UTC()
+		_, err := db.Exec("insert into "+config.WalletDatabase.TransactionsTable+" (wallet_id, date_and_time, currency, amount) values ($1, $2, $3, $4)", destination_wallet_id, transaction_date_time, currency, destination_initial_balance)
+		if err != nil {
+			t.Fatal("Error updating database.", err)
+		}
+
+		_, err = db.Exec("insert into "+config.WalletDatabase.BalanceTable+" (wallet_id, currency, balance) values ($1, $2, $3)", destination_wallet_id, currency, destination_initial_balance)
+		if err != nil {
+			t.Fatal("Error updating database.", err)
+		}
+	}
+
+	// Ensure balances and transactions table are empty when this unit test is finished
+	defer func() {
+		_, err = db.Exec("delete from " + config.WalletDatabase.BalanceTable)
+		if err != nil {
+			log.Fatal(err)
+		}
+		_, err = db.Exec("delete from " + config.WalletDatabase.TransactionsTable)
+		if err != nil {
+			log.Fatal(err)
+		}
+	}()
+
+	// Transfer some money from source to destination wallets
+	{
+		// Put message into requests queue
+		request_message := messages.POST_Transfer{
+			Header: messages.Header{
+				MessageID: message_id,
+				Action:    messages.Action_transfer,
+			},
+			SourceWalletID:      source_wallet_id,
+			DestinationWalletID: destination_wallet_id,
+			Amount:              transfer_amount,
+			Currency:            currency,
+		}
+		bytes_to_send, err := json.Marshal(request_message)
+		if err != nil {
+			t.Fatal("Could not serialise message.", err)
+		}
+		timeout := time.Duration(config.RequestsQueue.Timeout) * time.Second
+		queue_name := config.RequestsQueue.QueueName
+		timeout_context, cancel = context.WithTimeout(background_context, timeout)
+		_, err = requests_queue.LPush(timeout_context, queue_name, bytes_to_send).Result()
+		if err != nil {
+			cancel()
+			t.Fatal(err)
+		}
+		cancel()
+
+		// Wait for response from response queue
+		timeout = time.Duration(config.ResponsesQueue.Timeout) * time.Second
+		queue_name = config.ResponsesQueue.QueueName
+		timeout_context, cancel = context.WithTimeout(background_context, timeout)
+		string_slice, err := responses_queue.BRPop(timeout_context, timeout, queue_name).Result()
+		if err != nil {
+			cancel()
+			t.Fatal(err)
+		}
+		cancel()
+
+		// string_slice[0] gives the name of the queue
+		// string_slice[1] gives the data retrieved from the queue
+
+		// Deserialise JSON data received
+		response_message := responses.Transfer{}
+		err = json.Unmarshal([]byte(string_slice[1]), &response_message)
+		if err != nil {
+			t.Fatal(err)
+		}
+
+		// Verify response
+		expected_response := responses.Transfer{
+			Header: responses.Header{
+				MessageID: request_message.Header.MessageID,
+				Action:    request_message.Header.Action,
+			},
+			Status:     responses.Status_successful,
+			Currency:   request_message.Currency,
+			NewBalance: source_final_balance_str,
+		}
+		if !reflect.DeepEqual(response_message, expected_response) {
+			t.Fatal("Expected: ", expected_response, ", Got: ", response_message)
+		}
+
+		get_balance, err := db.Prepare("select balance from " + service.config.WalletDatabase.BalanceTable + " where wallet_id=$1")
+		if err != nil {
+			t.Fatal("Unable to prepare SQL statement.")
+		}
+		defer get_balance.Close()
+
+		// Verify balance of source wallet
+		var balance int64 = 0
+		err = get_balance.QueryRow(request_message.SourceWalletID).Scan(&balance)
+		if err != nil {
+			t.Fatal(err)
+		}
+		if balance != int64(source_final_balance) {
+			t.Error("Expected: ", source_final_balance, ", Got: ", balance)
+		}
+
+		// Verify balance of destination wallet
+		err = get_balance.QueryRow(request_message.DestinationWalletID).Scan(&balance)
+		if err != nil {
+			t.Fatal(err)
+		}
+		if balance != int64(destination_final_balance) {
+			t.Error("Expected: ", destination_final_balance, ", Got: ", balance)
+		}
+	}
 
 }
